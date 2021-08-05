239adadf155029047320a7783b2c1f212a2a9018
Tobias Ahrens <tobias.ahrens@zhinst.com>
2021-08-05 21:15:35 +0200

    card-game-war first try

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,35 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn play-round [player1-card player2-card]
+  (if (= player1-card player2-card)
+    -1
+    (if (= (.indexOf suits (.get player1-card 0)) (.indexOf suits  (.get player2-card 0)))
+      (if (> (.indexOf ranks (.get player1-card 1)) (.indexOf ranks (.get player2-card 1)))
+        1
+        2)
+      (if (< (.indexOf suits (.get player1-card 0)) (.indexOf suits  (.get player2-card 0)))
+        1
+        2))))
 
-(defn play-game [player1-cards player2-cards])
+(defn win-round [winner-stack looser-card stack]
+
+  (concat (rest winner-stack) stack [(first winner-stack)] [looser-card]))
+
+(defn init-game []
+  [(shuffle cards) (shuffle cards)])
+
+(defn play-game [player1-cards player2-cards]
+  (loop [p1 player1-cards p2 player2-cards stack []]
+  (prn p1)
+  (prn p2)
+  (prn stack)
+  (if (some empty? [p1 p2])
+    (if (empty? p1)
+      2
+      1
+    )
+    (case (play-round (first p1) (first p2))
+      -1 (recur (rest p1) (rest p2) (concat stack [(first p1)] [(first p2)]))
+      1  (recur (win-round p1 (first p2) stack) (rest p2) [])
+      2  (recur (rest p1) (win-round p2 (first p1) stack) [])))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -5,12 +5,23 @@
 
 ;; fill in  tests for your game
 (deftest test-play-round
-  (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings"))
+  (testing "the highest suit wins the cards in the round"
+    (is (= 1 (play-round [:spade 2] [:heart 2]))))
+  (testing "queens are higher rank than jacks"
+    (is (= 1 (play-round [:spade :queen] [:spade :jack]))))
+  (testing "kings are higher rank than queens"
+    (is (= 1 (play-round [:spade :king] [:spade :queen]))))
+  (testing "aces are higher rank than kings"
+    (is (= 1 (play-round [:spade :ace] [:spade :king])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (= 1 (play-game [[:spade 2][:club 3]] [[:club 2] [:spade 1]]))))
+  (testing "a complete game returns 1 or 2"
+      (is (some  #(= (apply play-game (init-game)) %) [1 2]))))
 
+(deftest test-init-game
+  (testing "the players have different cards"
+    (is (apply not= (init-game))))
+  (testing "the cards are shuffled"
+      (is (not= cards (first (init-game))))))
